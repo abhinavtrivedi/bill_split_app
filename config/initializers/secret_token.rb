8aed1dff0a7979dc46e4d4ef4c7959b1309a2b0d
Abhinav Trivedi <abhinavtrivedi@gmail.com>
2013-02-22 15:23:49 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BillSplitApp::Application.config.secret_token = '06dd57f3fe9d3f10ac0adab14ddf802ec39b35e892e29217c3d47bc5a932c5b2d22264e1da6496893c8649a176255698cb85cedf44dbb24f5401cad6fc448e86'
